ee9c10f94eb75ab0ab1f9ae1e54d39a8f6544054
ISABELLI LIMA SOARES <51054756821@info.local>
2024-05-10 10:17:22 -0300

    feat:exercios 6 e 7

--- ex4.js
+++ ex4.js
@@ -4,3 +4,33 @@
 //RECUPERAÇÃO para notas entre 5.0 e 7,0 
 //Ou a mensagem de REPROVADO para média inferior a 5,0.
 
+//receba 4 notas
+
+let nota1 = parseInt(prompt ("adicione a primeira nota: "))
+let nota2 = parseInt(prompt ("adicione a segunda nota: "))
+let nota3 = parseInt(prompt ("adicione a terceira nota: "))
+let nota4 = parseInt(prompt ("adicione a quarta nota: "))
+
+//some as notas
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log (media)
+
+//faça a media das notas
+
+//APROVADO = 7 ou maior
+//RECUPERAÇAO entre 5 e 7
+//REPROVADO menos que 5
+
+if (media >=7) {
+    console.log ("aprovado!")
+}
+
+else if (media > 4 && media < 7 ) {
+    console.log("recuperaçao")
+}
+
+else if (media < 5 ){
+    console.log ("reprovado")
+}
